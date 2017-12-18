ced7d854db99e798a0fcc306b8058a403e7f1469
fritzlim <fritzlim@gmail.com>
2017-12-19 01:28:09 +0800

    Initial commit

---
+++ 1 - Hello World/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Fritz Lim on 12/14/17.
+//  Copyright © 2017 Fritz Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
