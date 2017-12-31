3d728adb9824b495fc5b5d655cbc6f74bb3be10c
fritzlim <fritzlim@gmail.com>
2017-12-31 16:17:25 +0800

    Initial commit of iOS App #2: MCQ Quiz
    Added UI elements to main storyboard.

---
+++ 2 - MCQ Quiz/MCQ Quiz/MCQ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCQ Quiz
+//
+//  Created by Fritz Lim on 12/31/17.
+//  Copyright © 2017 Fritz Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
